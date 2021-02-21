ef91889f19b171ca4ee728338e623524749c34f1
giacomoS1 <giacomosinisc@gmail.com>
2021-02-20 23:19:38 -0600

    Fixed grounding check

--- Assets/PulsatingLight.cs
+++ Assets/PulsatingLight.cs
@@ -6,16 +6,16 @@ public class PulsatingLight : MonoBehaviour
 {
     private float time;
     private float n = 0;
-    private UnityEngine.Experimental.Rendering.Universal.Light2D light;
+    private UnityEngine.Experimental.Rendering.Universal.Light2D l;
     private void Start()
     {
-        light = GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+        l = GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
         time = Random.Range(0.04f, 0.08f);
     }
     private void FixedUpdate()
     {
         n += time;
-        light.intensity = Mathf.Abs(Mathf.Sin(n));
+        l.intensity = Mathf.Abs(Mathf.Sin(n));
     }
 }
 
