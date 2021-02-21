328bb24b953a2ebfea32408f730b838a838154fc
giacomoS1 <giacomosinisc@gmail.com>
2021-02-20 19:28:37 -0600

    Level 1 Finished
    Change bottom cliff to polygon collider, added lights to direct player

---
+++ Assets/PulsatingLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+public class PulsatingLight : MonoBehaviour
+{
+    private float time;
+    private float n = 0;
+    private UnityEngine.Experimental.Rendering.Universal.Light2D light;
+    private void Start()
+    {
+        light = GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+        time = Random.Range(0.04f, 0.08f);
+    }
+    private void FixedUpdate()
+    {
+        n += time;
+        light.intensity = Mathf.Abs(Mathf.Sin(n));
+    }
+}
